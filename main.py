4736a080c52312709c9344d1e9e5671fdb19d131
Софія Бардакова <sofiia.bardakova@nure.ua>
2026-03-05 21:20:20 +0200

    feat: add equals, clear and parentheses buttons to calculator fix: adjust grid positions for -, *, / buttons

--- main.py
+++ main.py
@@ -56,11 +56,23 @@ btn_plus = tk.Button(root, text="+", command=lambda: add_to_calculation("+"), wi
 btn_plus.grid(row=2, column=4)
 
 btn_minus = tk.Button(root, text="-", command=lambda: add_to_calculation("-"), width=5, font=("Arial", 14))
-btn_minus.grid(row=5, column=2)
+btn_minus.grid(row=3, column=4)
 
 btn_mul = tk.Button(root, text="*", command=lambda: add_to_calculation("*"), width=5, font=("Arial", 14))
-btn_mul.grid(row=5, column=2)
+btn_mul.grid(row=4, column=4)
 
 btn_div = tk.Button(root, text="/", command=lambda: add_to_calculation("/"), width=5, font=("Arial", 14))
-btn_div.grid(row=5, column=2)
+btn_div.grid(row=5, column=4)
+
+btn_open = tk.Button(root, text="(", command=lambda: add_to_calculation("("), width=5, font=("Arial", 14))
+btn_open.grid(row=5, column=1)
+
+btn_close = tk.Button(root, text=")", command=lambda: add_to_calculation(")"), width=5, font=("Arial", 14))
+btn_close.grid(row=5, column=3)
+
+btn_equals = tk.Button(root, text="=", command=lambda: evaluate_calculation, width=11, font=("Arial", 14))
+btn_equals.grid(row=6, column=3, columnspan=2)
+
+btn_clear = tk.Button(root, text="C", command=clear_field, width=11, font=("Arial", 14))
+btn_clear.grid(row=6, column=1, columnspan=2)
 root.mainloop()
